68069622b40a6e678472d2d02d683e121c0546c0
Dylan Nicolet <megadylan1@hotmail.com>
2026-02-03 16:04:21 +0400

    [EVO] Completly removed Redux from the project

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,22 +1,7 @@
-import React from 'react';
 import {Outlet} from "react-router-dom";
 import Header from '../layout/Header';
-import { useDispatch } from "react-redux"
-import { updateScreenWidth } from "../redux/webConfigSlice";
-
 
 export default function App() {
-  const dispatch = useDispatch()
-
-  //Screen width tracker (Updates redux store live)
-  React.useEffect(() => {
-    function handleResize(){
-      dispatch(updateScreenWidth(window.innerWidth))
-    }
-
-    window.addEventListener("resize", handleResize)
-  },[])
-
   return (
     <section>
       <Header />
